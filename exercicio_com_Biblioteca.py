7a197119f0fd10fe3261ca7702274194ce8dbc01
ruanfelix675 <ruanfeliz7777@gmail.com>
2026-03-24 16:43:38 -0300

    Create exercicio_com_Biblioteca.py

---
+++ exercicio_com_Biblioteca.py
@@ -0,0 +1,6 @@
+import math 
+n = float(input('digita numeros decimais'))
+print('raiz quadrada', math.sqrt(n)  )
+print('arendodamento pra cima ', math.ceil(n))
+print('arendodamento pra baixo',math.floor(n))
+print('ao quadrado',n ** 2)
