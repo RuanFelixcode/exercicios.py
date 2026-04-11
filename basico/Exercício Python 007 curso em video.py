cc76ae7f7ade0fa215168c4677aff39466df6499
ruan_void7777 <ruanfeliz7777@gmail.com>
2026-04-11 10:10:30 -0300

    Create Exercício Python 007 curso em video.py

---
+++ basico/Exercício Python 007 curso em video.py
@@ -0,0 +1,6 @@
+'''Exercício Python 007: Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+'''
+n = float(input('digite sua nota:'))
+n2 = float(input('digite sua nota:'))
+media = (n + n2)/2
+print(f'media {media:.1f}')
