a57f1d27fc49175d4b6bdcfa1f561ebe48bca5b6
ruan_void7777 <ruanfeliz7777@gmail.com>
2026-04-11 10:01:10 -0300

    Create Exercício Python 006 curso em video.py

---
+++ basico/Exercício Python 006 curso em video.py
@@ -0,0 +1,4 @@
+'''Exercício Python 006: Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.'''
+
+n = int(input('digite um numero:'))
+print('valor digitado {} o dobro e {} o triplo e {} raiz quadrada e {:.2f}'.format(n,n*2,n*3,n**0.5))
