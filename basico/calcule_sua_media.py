41c7d0a29bc478a759e57aa0e55772210611fb58
ruan_void7777 <ruanfeliz7777@gmail.com>
2026-04-15 15:28:41 -0300

    Update calcule_sua_media.py

--- basico/calcule_sua_media.py
+++ basico/calcule_sua_media.py
@@ -1,4 +1,6 @@
 n1 = float(input('sua nota' ))
 n2 = float(input('sua segunda hota' ))
-print('sua primeira nota foi: ',n1/2)
-print('sua primeira nota foi:',n2/2)
+print(f'sua primeira nota foi: {n1}')
+print(f'sua primeira nota foi: {n2}')
+soma = (n1 + n2)/2
+print(f'resultado {soma}')
