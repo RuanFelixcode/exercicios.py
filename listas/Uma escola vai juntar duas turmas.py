f98c57ba52b5f1eaf5c8d8196638a9065cbdff92
ruan_void7777 <ruanfeliz7777@gmail.com>
2026-04-17 12:38:11 -0300

    Create Uma escola vai juntar duas turmas.py

---
+++ listas/Uma escola vai juntar duas turmas.py
@@ -0,0 +1,8 @@
+'''Uma escola vai juntar duas turmas:
+turmaA = ["Ana", "Bruno"]
+turmaB = ["Carlos", "Daniela"]
+Adicione todos os alunos da turmaB na turmaA.'''
+turmaA = ["Ana", "Bruno"]
+turmaB = ["Carlos", "Daniela"]
+turmaA += turmaB
+print(turmaA)
