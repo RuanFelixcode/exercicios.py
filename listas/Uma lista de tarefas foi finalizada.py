7b03e246a7b608d2f95fef1635756a5417a67e25
ruan_void7777 <ruanfeliz7777@gmail.com>
2026-04-19 16:33:54 -0300

    Create Uma lista de tarefas foi finalizada.py

---
+++ listas/Uma lista de tarefas foi finalizada.py
@@ -0,0 +1,7 @@
+'''Uma lista de tarefas foi finalizada:
+tarefas = ["Estudar", "Trabalhar", "Dormir"]
+Esvazie completamente essa lista.'''
+tarefas = ["Estudar", "Trabalhar", "Dormir"]
+print(tarefas)
+tarefas.clear()
+print(f'lista vazia {tarefas}')
