97244c84d923047e974b3ba767804c25a78b9418
ruan_void7777 <ruanfeliz7777@gmail.com>
2026-04-19 16:38:26 -0300

    Create Uma lista contém diferentes tipos de dados.py

---
+++ listas/Uma lista contém diferentes tipos de dados.py
@@ -0,0 +1,10 @@
+'''Uma lista contém diferentes tipos de dados:
+dados = ["Matheus", 39, 1.55, True]
+
+Crie um programa que:
+• Mostre cada elemento separadamente
+• Mostre a quantidade de elementos'''
+dados = ["Matheus", 39, 1.55, True]
+for i in dados:
+    print(i)
+print(len(dados))
