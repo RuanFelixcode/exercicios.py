9b2229d8f4272a16de4f74788d4d7f732f40d612
ruan_void7777 <ruanfeliz7777@gmail.com>
2026-04-21 13:31:49 -0300

    Create quantidade de peças produzidas por dia.py

---
+++ basico/quantidade de peças produzidas por dia.py
@@ -0,0 +1,3 @@
+pecas = int(input('digite a quantidade de peças produzidas:'))
+quant_dia = int(input('digite a quantidade de dias :'))
+print(f'quantidade de peças produzidas por dia {pecas//quant_dia}')'''
