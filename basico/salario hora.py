7f641fab5320bb3c87c7f5b210d3ed9c070a8439
ruan_void7777 <ruanfeliz7777@gmail.com>
2026-04-21 13:26:35 -0300

    Create salario hora.py

---
+++ basico/salario hora.py
@@ -0,0 +1,3 @@
+salario_mensal = float(input('digite seu salario mensal:'))
+horas_trabalhadas = float(input('digite o total de horas trabalhadas:'))
+print(f'seu salario mensal {salario_mensal} total de horas trabalhadas {horas_trabalhadas} o quanto ganha por hora {salario_mensal/horas_trabalhadas}')
