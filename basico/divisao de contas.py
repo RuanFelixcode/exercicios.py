bf18f65758dfaac4577a8b1b7dbd264ea55df3ef
ruan_void7777 <ruanfeliz7777@gmail.com>
2026-04-21 13:28:05 -0300

    Create divisao de contas.py

---
+++ basico/divisao de contas.py
@@ -0,0 +1,3 @@
+valor = float(input('digite o valor total:'))
+total_pessoas = float(input('digite a quantidade de pessoas que vao receber:'))
+print(f'o valor e {valor} dividindo pra {total_pessoas} pessoas cada uma fica com {valor/total_pessoas}')
