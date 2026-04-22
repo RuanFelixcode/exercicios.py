a74bc001bd1f7917bee3a7982bf23e52264a5134
ruan_void7777 <ruanfeliz7777@gmail.com>
2026-04-22 18:25:44 -0300

    Create mini Sistema de Desconto.py

---
+++ funçoes/mini Sistema de Desconto.py
@@ -0,0 +1,12 @@
+'''Sistema de Desconto 
+Requisitos:
+receber preço
+receber % de desconto
+mostrar preço final
+Função:
+calcular_desconto(preco, desconto) 
+'''
+def calcular_desconto ():
+    preco = float(input('digite o preço:'))
+    print(f'desconto {preco*0.10}')
+calcular_desconto ()   
